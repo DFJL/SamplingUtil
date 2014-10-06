1e7183cce4db8a313bf3022fdac95a70ce3dfa1e
DFJL <felipe_jimenez_l@yahoo.com>
2014-10-05 21:28:35 -0600

    Cambios en función nsize()

--- R/nsize.R
+++ R/nsize.R
@@ -10,14 +10,14 @@
 #' @return lista con n0 y n ajustado.
 #' @export
 #' @description Basado en slides del curso Topicos de Muestreo.Universidad de Costa Rica.
-nsize<-function(df,x,r,alpha=0.05){
+nsize<-function(x,r,alpha=0.05){
   #Calculo de componentes de la formula
   z<-qnorm(1-(alpha/2))
-  s<- sd(df$x)
-  errorabs<-r*mean(df$x)
+  s<- sd(x)
+  errorabs<-r*mean(x)
   #Calculo de tamaño de muestra
   n0<- round((z*s/errorabs)^2,0)
-  n<-round((n0/(1+(n0/length(df$x)))))
+  n<-round((n0/(1+(n0/length(x)))))
   sizes<- list(n0=n0,n_adjust=n)
   return(sizes)
   
