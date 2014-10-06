f2ec2249f7e2a648588e7095891b694859dde8b6
DFJL <felipe_jimenez_l@yahoo.com>
2014-10-05 20:44:36 -0600

    Con primera versión de función nsize

--- R/nsize.R
+++ R/nsize.R
@@ -1,21 +1,21 @@
 
 
-#'Cálculo de tamaños muestrales
+#'Calculo de tamaños muestrales
 #'
-#' Función para generar una muestra con control de error absoluto o relativo.
+#' Funcion para generar una muestra con control de error absoluto o relativo.
 #' @param df: Data frame con datos para calcular media y variancia.
 #' @param x: Columna base para calcular tamaño de muestra.
 #' @param r: Error relativo.
 #' @param alpha: Riesgo alpha de que el error real supere r.
 #' @return lista con n0 y n ajustado.
 #' @export
-#' @description Basado en slides del curso Tópicos de Muestreo.Universidad de Costa Rica.
+#' @description Basado en slides del curso Topicos de Muestreo.Universidad de Costa Rica.
 nsize<-function(df,x,r,alpha=0.05){
-  #Cálculo de componentes de la fórmula
+  #Calculo de componentes de la formula
   z<-qnorm(1-(alpha/2))
   s<- sd(df$x)
   errorabs<-errorrel*mean(df$x)
-  #Cálculo de tamaño de muestra
+  #Calculo de tamaño de muestra
   n0<- round((z*s/errorabs)^2,0)
   n<-round((n0/(1+(n0/length(df$x)))))
   sizes<- list(n0=n0,n_adjust=n)
