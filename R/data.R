320dba4c485be4312975c51e00a9e1aeadae7ebd
DFJL <felipe_jimenez_l@yahoo.com>
2014-10-06 20:23:38 -0600

    Cambio en la forma de cargar datasets

--- R/data.R
+++ R/data.R
@@ -1,22 +1,8 @@
 
-#Cargando la librería para obtener los datos
-library(foreign)
-
-#Seteando directorio donde se encuentran los datos
-
-dir<- c("C:/Datos/SamplingUtil_package/SamplingUtil")
-setwd(dir)
-
-# Cargando la data de la ENAHO 2013
-ENAHO2013<- read.spss("ENAHO 2013 EXPERTOS.sav",to.data.frame = TRUE)
-
-#' Cargar data de la ENAHO 2013
-#' 
-#' Carga en un dataframe la data de la ENAHO 2013
-#' @param No requiere argumentos.
+#' ENAHO 2013.
+#'
+#' Data de la Encuesta Nacional de Hogares 2013
+#'
 #' @format Dataframe con la data ENAHO 2013.
 #' @description Para el detalle y diccionario de datos ir a la direccion:http://www.inec.go.cr/anda4/index.php/catalog/130/accesspolicy
-#' @export
-ENAHO_2013<- function(){
-  return(ENAHO2013)
-}
+"ENAHO2013"
