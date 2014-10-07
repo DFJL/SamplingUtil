455ba18a826101716afd5f962ca93fcabbbbc2c5
DFJL <felipe_jimenez_l@yahoo.com>
2014-10-06 23:02:21 -0600

    Cambios para hacer más liviano el paquete.

--- data-raw/ENAHO2013.R
+++ data-raw/ENAHO2013.R
@@ -2,15 +2,13 @@
 #Cargando la librería para obtener los datos
 library(foreign)
 
-#Seteando directorio donde se encuentran los datos
+#Seteando directorio donde se encuentran los datos(Se deben incluir al directorio raiz del proyecto)
 
-dir<- c("C:/Datos")
+dir<- c("C:/Datos/SamplingUtil/data-raw")
 setwd(dir)
 
 # Cargando la data de la ENAHO 2013
 ENAHO2013<- read.spss("ENAHO 2013 EXPERTOS.sav",to.data.frame = TRUE)
 
-enaho2<- sample(1:nrow(ENAHO2013),1000)
-enaho2<- ENAHO2013[enaho2,]
 
 save(ENAHO2013, file = "data/ENAHO2013.rdata")
