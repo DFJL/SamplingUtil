355a2d185c38a285a276e271345314936e547a92
DFJL <felipe_jimenez_l@yahoo.com>
2014-10-11 21:01:06 -0600

    Agrega cálculo de tamaño de muestra óptimo por estrato en nstrata.

--- R/nstrata.R
+++ R/nstrata.R
@@ -4,15 +4,20 @@
 #' Funcion para distribuir la muestra en los estratos.
 #' @param n: Tamaño de la muestra total.
 #' @param wh: Objeto on las proporciones por estrato.
+#' @param sh: Desviación de cada estrato.
+#' @param ch: Costo por entrevista .
 #' @param method: Método de asignación de la muestra a los estratos.
-#' @param alpha: Riesgo alpha de que el error real supere r.
 #' @return Objeto con muestra distribuida por estrato.
 #' @export
 #' @references Basado en slides del curso Topicos de Muestreo.Universidad de Costa Rica.
-nstrata<-function(n,wh,method=c("proportional")){
+nstrata<- function(n,wh,sh=0,ch=0,method=c("proportional","optimal")){
   nh<-rep(0,length(wh))
   if (method=="proportional"){
-    nh<-ceiling(n*wh)
+    nh<-ceiling(n*wh)}
+  else{
+    if(method=="optimal" && ch!=0){
+      nh<-ceiling(n*((wh*sh)/sqrt(ch))/(sum(((wh*sh)/sqrt(ch)))))
+    }
   }
   return(nh)
 }
